7c265ddf0f40c6fec1e946c3138db8b99dd4dba7
Felix Gehring <github@felix-gehring.de>
2020-03-22 20:52:53 +0100

    Correct viewport height

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+setCorrectViewportHeight();
+
+function setCorrectViewportHeight() {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty(
+    "--app-viewport-height",
+    `${vh}px`
+  );
+  console.log(`${vh}px`);
+}
+
 function pullHandle() {
   window.scrollBy({
     top: 400,
