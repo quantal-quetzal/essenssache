5db2fe491ef8f2b6e953e544356c5d588333129e
Felix Gehring <github@felix-gehring.de>
2020-05-15 20:40:09 +0200

    Titel

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function pullHandle() {
   window.scrollBy({
     top: 400,
-    behavior: "smooth"
+    behavior: "smooth",
   });
+}
+
+function hideArrow() {
+  document.getElementById("arrow").style.opacity = 1 - window.scrollY / 100;
+  document.getElementById("arrow").style.height = `${
+    2 - window.scrollY / 100
+  }rem`;
+  document.getElementById("pullHandle").style.paddingTop = `${
+    1.5 - window.scrollY / 100
+  }rem`;
 }
